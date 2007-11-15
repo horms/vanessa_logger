512484fc986aea495cd0cdfdffe5ac9d7d0ebc24
Simon Horman <horms@verge.net.au>
2007-11-16 04:14:26 +0900

    Update autogen.sh from peridtion

--- autogen.sh
+++ autogen.sh
@@ -8,33 +8,37 @@ test -z "$srcdir" && srcdir=.
 THEDIR=`pwd`
 cd $srcdir
 
-RC=0
+DIE=0
 
-gnu="ftp://ftp.gnu.org/pub/gnu/"
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have autoconf installed to compile vanessa_logger."
+	echo "Download the appropriate package for your distribution,"
+	echo "or get the source from ftp://ftp.gnu.org/pub/gnu/autoconf/"
+	DIE=1
+}
 
-for command in autoconf automake libtoolize; do
-        pkg=$command
-        case $command in
-                libtoolize) pkg=libtool;;
-        esac
-        URL=$gnu/$pkg/
-        if ! $command --version </dev/null >/dev/null 2>&1; then
-                RC=$?
-cat << !EOF >&2
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have automake installed to compile vanessa_logger."
+	echo "Download the appropriate package for your distribution,"
+	echo "or get the source from ftp://ftp.gnu.org/pub/gnu/automake/"
+	DIE=1
+}
 
-You must have $pkg installed to compile the vanessa_logger.  
-Download the appropriate package for your system, 
-or get the source tarball at: $URL
-!EOF
-        fi
-done
+(libtoolize --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have libtool installed to compile vanessa_logger."
+	echo "Download the appropriate package for your distribution,"
+	echo "or get the source from ftp://ftp.gnu.org/pub/gnu/libtool/"
+	DIE=1
+}
 
-if [ $RC != 0 ]; then
-        exit $RC
+if test "$DIE" -eq 1; then
+	exit 1
 fi
 
-
-if [ -z "$*" ]; then
+if test -z "$*"; then
 	echo "I am going to run ./configure with no arguments - if you wish "
         echo "to pass any to it, please specify them on the $0 command line."
 fi
@@ -48,9 +52,10 @@ aclocal $ACLOCAL_FLAGS
 (autoheader --version)  < /dev/null > /dev/null 2>&1 && autoheader
 automake --add-missing $am_opt
 autoconf
+libtoolize --ltdl
 cd $THEDIR
 
-$srcdir/configure "$@" || exit -1
+$srcdir/configure "$@" || exit $?
 
-echo 
+echo
 echo "Now type 'make' to compile vanessa_logger."
